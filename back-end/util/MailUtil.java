0fd79376d368632ccc8faabb33df6a3533496e73
Twilighce <rbing@mail.ustc.edu.cn>
2018-09-12 11:59:53 +0800

    Update MailUtil.java

--- back-end/util/MailUtil.java
+++ back-end/util/MailUtil.java
@@ -75,7 +75,7 @@ public class MailUtil implements Callable<Boolean>{
 			    // receiver
 			    message.addRecipient(Message.RecipientType.TO, new InternetAddress(email));
 			    // 
-			    message.setSubject("Stubconnect Verification Code");
+			    message.setSubject("Verification Code");
 			    String content = "<html><head></head><body><h2>The verification code is: " + code +"</h2></body></html>";
 
 			    message.setContent(content, "text/html;charset=UTF-8");
@@ -114,7 +114,7 @@ public class MailUtil implements Callable<Boolean>{
 	      	// auth info，
 	      		public PasswordAuthentication getPasswordAuthentication() {      			
 	      		// 
-	        		return new PasswordAuthentication("stubhubconnect", "ice_19941024");
+	        		return new PasswordAuthentication("xxx", "ice_19941024");
 	        	}
 			});
 
